f55d79b308577820faec6c8e4f22a94bef4df468
Diana Buffone <diana.pamelaet@gmail.com>
2023-06-03 21:16:50 -0400

    Update student class

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
 class Student
- 
+    attr_reader :name, :age, :scores
+
+    def initialize(personal_info)
+        @name = personal_info[:name]
+        @age = personal_info[:age]
+        @scores = []
+    end
+
+    def log_score(score)
+        @scores.push(score)
+    end
+
+    def grade
+        @scores.sum(0.0) / @scores.size
+    end
 end
