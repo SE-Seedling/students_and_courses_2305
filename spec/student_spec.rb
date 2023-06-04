ed26a1dfee537bd7df3275b7bd4c22a7c8193d96
Diana Buffone <diana.pamelaet@gmail.com>
2023-06-03 21:34:05 -0400

    Update student spec structure

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -12,21 +12,21 @@ RSpec.describe Student do
         it 'initializes with empty scores' do
             expect(subject.scores).to be_an(Array)
         end
+    end
 
-        describe '#log_score' do
-            it 'adds scores for student' do
-                subject.log_score(89)
-                subject.log_score(78)
-                expect(subject.scores).to match_array([89,78])
-            end
+    describe '#log_score' do
+        it 'adds scores for student' do
+            subject.log_score(89)
+            subject.log_score(78)
+            expect(subject.scores).to match_array([89,78])
         end
+    end
 
-        describe '#grade' do
-            it 'return the average grade' do
-                subject.log_score(89)
-                subject.log_score(78)
-                expect(subject.grade).to eq(83.5)
-            end
+    describe '#grade' do
+        it 'return the average grade' do
+            subject.log_score(89)
+            subject.log_score(78)
+            expect(subject.grade).to eq(83.5)
         end
     end
 end
