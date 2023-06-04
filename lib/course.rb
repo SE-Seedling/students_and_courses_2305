a153d13e33d7760e6e79b73b8288e4e026e14fe7
Diana Buffone <diana.pamelaet@gmail.com>
2023-06-03 22:06:15 -0400

    Update course class

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,18 @@
 class Course
+    attr_reader :name, :capacity, :students
 
+    def initialize(name, capacity)
+        @name = name
+        @capacity = capacity
+        @students = []
+    end
+
+    def full?
+        return true if @students.size == @capacity
+        false
+    end
+
+    def enroll(student_object)
+        @students << student_object
+    end
 end
